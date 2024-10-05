7dcbcfe75a62f04d52abab37a2c9d1047ab8ffa4
AyaMagdy27 <ayamado1996@gmail.com>
2024-10-05 05:31:24 +0300

    side nav

--- js/menu.js
+++ js/menu.js
@@ -1,40 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const productsLink = document.getElementById('productsLink');
-    const productsList = document.getElementById('prodactsList');
+/* Open the side navigation */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
 
-    function toggleProductsMenu() {
-        if (productsList.classList.contains('visible')) {
-            // If menu is visible, hide it
-            productsList.classList.remove('visible');
-            productsList.classList.add('hidden');
-            
-            // Remove 'hidden' class and set display to 'none' after animation
-            setTimeout(() => {
-                productsList.classList.remove('hidden');
-                productsList.style.display = 'none';
-            }, 2000); // Match this to your CSS animation duration (2s)
-        } else {
-            // If menu is hidden, show it
-            productsList.style.display = 'block';
-            
-            // Force a reflow before adding the 'visible' class
-            void productsList.offsetWidth;
-            
-            productsList.classList.add('visible');
-        }
-    }
-
-    productsLink.addEventListener('click', function(event) {
-        event.stopPropagation();
-        toggleProductsMenu();
-    });
-
-    // Close the menu if clicking outside
-    document.addEventListener('click', function(event) {
-        if (!productsLink.contains(event.target) && !productsList.contains(event.target)) {
-            if (productsList.classList.contains('visible')) {
-                toggleProductsMenu();
-            }
-        }
-    });
-});
+/* Close the side navigation */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
